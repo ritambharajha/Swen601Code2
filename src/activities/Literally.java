d07f982fa9415d395d0d08b0178c5034beef54ab
Ritambhara Jha <ritambhara@Ritambharas-MacBook-Air.local>
2021-08-27 10:50:07 -0400

    Variables classwork

---
+++ src/activities/Literally.java
@@ -0,0 +1,17 @@
+package activities;
+
+public class Literally {
+    public static void main (String[] args){
+        int n = 10;
+        double d = 10.0;
+        char c = 'a';
+        String s = "Hello";
+        boolean b = true;
+        System.out.println(n);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(b);
+    }
+
+}
